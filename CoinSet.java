c5fad9a9ecd96bd288cb0d23cd32e885b66405f3
Lou1sThomas <164386461+Lou1sThomas@users.noreply.github.com>
2024-11-04 21:31:22 +0000

    Add files via upload

---
+++ CoinSet.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+/**
+   A set of coins.
+*/
+public class CoinSet
+{  
+   private ArrayList<Coin> set;
+
+   /**
+      Constructs a CoinSet object.
+   */
+   public CoinSet()
+   {  
+      set = new ArrayList<Coin>();
+   }
+   //ADD REMAINING CODE HERE
+}
---
+++ VendingMachine.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+/**
+   A vending machine.
+*/
+public class VendingMachine
+{  
+   private ArrayList<Product> products;
+   public CoinSet coins;
+   public CoinSet currentCoins;
+
+   /**
+      Constructs a VendingMachine object.
+   */
+   public VendingMachine()
+   { 
+      products = new ArrayList<Product>();
+      coins = new CoinSet();
+      currentCoins = new CoinSet();
+   }
+   //ADD REMAINING CODE HERE
+}
